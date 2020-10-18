b4956855802dc4b43149144937cb4cd8a58711c1
masayoshi4649 <masayoshi4649@gmail.com>
2020-10-18 16:44:59 +0900

    HTML上に表示するように改善

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"runtime"
 
 	"github.com/gin-gonic/gin"
 	"github.com/olahol/melody"
@@ -15,28 +16,30 @@ func main() {
 	router := gin.Default()
 	m := melody.New()
 
-	router.GET("/", func(ctx *gin.Context) {
+	go router.GET("/", func(ctx *gin.Context) {
 		http.ServeFile(ctx.Writer, ctx.Request, "index.html")
 	})
 
-	router.GET("/ws", func(ctx *gin.Context) {
+	go router.GET("/ws", func(ctx *gin.Context) {
 		m.HandleRequest(ctx.Writer, ctx.Request)
 	})
 
-	m.HandleMessage(func(s *melody.Session, msg []byte) {
+	go m.HandleMessage(func(s *melody.Session, msg []byte) {
 		m.Broadcast(msg)
 	})
 
-	m.HandleConnect(func(s *melody.Session) {
+	go m.HandleConnect(func(s *melody.Session) {
 		log.Printf("websocket connection open. [session: %#v]\n", s)
 	})
 
-	m.HandleDisconnect(func(s *melody.Session) {
+	go m.HandleDisconnect(func(s *melody.Session) {
 		log.Printf("websocket connection close. [session: %#v]\n", s)
 	})
 
 	// Listen and server on 0.0.0.0:8989
+	fmt.Println(runtime.NumGoroutine())
 	router.Run(":8989")
 
 	fmt.Println("Websocket App End.")
+
 }
